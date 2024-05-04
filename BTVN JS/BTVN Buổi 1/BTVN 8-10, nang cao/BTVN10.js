ff2092fe9b437fba8f59df9484d2ee39e4eeb280
Baophucxz20040924 <baophucxz2004@gmail.com>
2024-05-04 10:36:03 +0700

    projectJS

---
+++ BTVN JS/BTVN Buổi 1/BTVN 8-10, nang cao/BTVN10.js
@@ -0,0 +1,7 @@
+let a=2024
+if (a%4==0){
+    console.log("Nam nhuan");
+}
+else {
+    console.log("Nam thuong");
+}
---
+++ BTVN JS/BTVN Buổi 1/BTVN 8-10, nang cao/BTVN9.js
@@ -0,0 +1,10 @@
+let a="K"
+if (a>="A" && a<="Z"){
+    console.log(a+" la chu cai viet hoa");
+}
+else if (a>="a"&& a<="z"){
+    console.log(a+" la chu cai viet thuong");
+}
+else{
+    console.log(a+" khong phai la chu cai");
+}
